8350a7d26f5e43e693174cdde9daebffd9d30ef0
tandonglei <tandonglei@elastos.org>
2018-07-02 14:35:01 +0800

    modify the web service port

--- src/main/java/org/elastos/elaweb/HttpServer.java
+++ src/main/java/org/elastos/elaweb/HttpServer.java
@@ -9,7 +9,7 @@ import org.eclipse.jetty.server.Server;
  */
 public class HttpServer {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8988);
+        Server server = new Server(8989);
         server.setHandler(new ElaHandle());
         server.start();
         server.join();
